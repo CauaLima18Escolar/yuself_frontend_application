c819eeeca6c5e50a23a8a92391859e931f0540b2
Cauã de Lima Araújo <20231144010003@ifrn.local>
2026-09-22 16:20:34 -0300

    feat: adiciona dialog para listar itens do carrinho

--- src/app/components/shopping-cart/shopping-cart.ts
+++ src/app/components/shopping-cart/shopping-cart.ts
@@ -1,16 +1,30 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CartService } from '../../service/cart-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-shopping-cart',
   styleUrl: './shopping-cart.css',
   templateUrl: './shopping-cart.html',
 })
 export class ShoppingCart {
   #cartService = inject(CartService);
+  protected isOpen = false
+
+  public onClose() {
+    this.isOpen = false
+  }
+
+  public onOpen() {
+    this.isOpen = true
+  }
 
   public getItensAmount(): number {
     return this.#cartService.getTotalOfItems();
+  }
+
+  public getService(): CartService {
+    return this.#cartService
   }
 }
